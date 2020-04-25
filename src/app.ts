2f76f7751430e6eddff3e790b0c3e71942af1f81
Andrei <webconsult.ekb@gmail.com>
2020-04-25 23:55:36 +0200

    fix headers

--- src/app.ts
+++ src/app.ts
@@ -43,9 +43,6 @@ app.use((req, res, next) => {
 });
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
     next();
 });
 
